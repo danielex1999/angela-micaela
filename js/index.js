bdefffaab155114ca5ac3288dccad5188a1da1cf
danielex1999 <daniel_jul_can@hotmail.com>
2026-03-15 18:52:23 -0500

    Changed 8M to Sunflower

--- js/index.js
+++ js/index.js
@@ -1,7 +1,35 @@
-function abrirNotas(){
-    document.getElementById("notes-popup").style.display="flex";
+function abrirNotas() {
+    document.getElementById("notes-popup").style.display = "flex";
 }
 
-function cerrarNotas(){
-    document.getElementById("notes-popup").style.display="none";
+function cerrarNotas() {
+    document.getElementById("notes-popup").style.display = "none";
 }
+
+
+
+
+const ruta = "assets/img/moments/";
+
+const imagenes = [
+    "🎂 Kitty cumpleañera.jpeg",
+    "💇‍♀️ Balayage.jpeg"
+];
+
+
+imagenes.sort(() => Math.random() - 0.5);
+
+const contenedores = document.querySelectorAll(".img-container-moment");
+
+contenedores.forEach((container, index) => {
+
+    const img = container.querySelector("img");
+    const p = container.querySelector("p");
+
+    const nombreArchivo = imagenes[index];
+
+    img.src = ruta + nombreArchivo;
+
+    let nombre = nombreArchivo.split(".")[0];
+    p.textContent = nombre;
+});
