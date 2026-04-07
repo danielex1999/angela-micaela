3bc6c020aafc42254cfff605210ca2e73d0961de
Juan Cavero <daniel_jul_can@hotmail.com>
2026-04-06 21:40:00 -0500

    test

--- js/Pages/calendar.js
+++ js/Pages/calendar.js
@@ -4,4 +4,16 @@ const navBarMenu = document.querySelector(".navbar-menu");
 navBarToogle.addEventListener("click", () => {
     navBarMenu.classList.toggle("active");
     navBarToogle.classList.toggle("active");
-});    
+});
+
+
+/*Abrir Notas Seminario de Investigacion*/
+function abrirNotasSeminario() {
+    document.getElementById("notes-popup-semi-invest").style.display = "flex";
+    document.getElementsByClassName("navbar")[0].style.display = "none";
+}
+
+function cerrarNotasSeminario() {
+    document.getElementById("notes-popup-semi-invest").style.display = "none";
+    document.getElementsByClassName("navbar")[0].style.display = "flex";
+}
