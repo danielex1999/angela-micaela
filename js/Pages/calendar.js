540d7579fa8082382661bbee8e18276213bb70b5
Juan Cavero <daniel_jul_can@hotmail.com>
2026-04-06 21:45:44 -0500

    test

--- js/Pages/calendar.js
+++ js/Pages/calendar.js
@@ -1,5 +1,6 @@
 const navBarToogle = document.querySelector(".navbar-toggle");
 const navBarMenu = document.querySelector(".navbar-menu");
+const slots = document.getElementsByClassName("slot");
 
 navBarToogle.addEventListener("click", () => {
     navBarMenu.classList.toggle("active");
@@ -11,9 +12,15 @@ navBarToogle.addEventListener("click", () => {
 function abrirNotasSeminario() {
     document.getElementById("notes-popup-semi-invest").style.display = "flex";
     document.getElementsByClassName("navbar")[0].style.display = "none";
+    for (let i = 0; i < slots.length; i++) {
+        slots[i].style.display = "none";
+    }
 }
 
 function cerrarNotasSeminario() {
     document.getElementById("notes-popup-semi-invest").style.display = "none";
     document.getElementsByClassName("navbar")[0].style.display = "flex";
+    for (let i = 0; i < slots.length; i++) {
+        slots[i].style.display = "flex";
+    }
 }
