4389c560bd3c63d2ff4f3ebe0d5ff3760f0bd6fc
Juan Cavero <daniel_jul_can@hotmail.com>
2026-04-06 22:51:55 -0500

    test

--- js/Pages/calendar.js
+++ js/Pages/calendar.js
@@ -24,3 +24,20 @@ function cerrarNotasSeminario() {
         slots[i].style.display = "flex";
     }
 }
+
+/*Abrir Notas Psicopatologia*/
+function abrirNotasPsicopatologia() {
+    document.getElementById("notes-popup-psicopatology").style.display = "flex";
+    document.getElementsByClassName("navbar")[0].style.display = "none";
+    for (let i = 0; i < slots.length; i++) {
+        slots[i].style.display = "none";
+    }
+}
+
+function cerrarNotasPsicopatologia() {
+    document.getElementById("notes-popup-psicopatology").style.display = "none";
+    document.getElementsByClassName("navbar")[0].style.display = "flex";
+    for (let i = 0; i < slots.length; i++) {
+        slots[i].style.display = "flex";
+    }
+}
